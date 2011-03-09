6ec73f9a87c159b2b35cc22ca4677b57ca78b01c
KonradZych <dominesum@gmail.com>
2011-03-09 15:44:28 +0100

    Function to divide markers into groups and save into cross object format, a lot of improvments in functions

--- R/core.r
+++ R/core.r
@@ -1,3 +1,4 @@
+#WORK here
 check_parameters <- function(functionName, numericParameters, verboseParameter, debugModeParameter, verbose=FALSE, debugMode=0){
 	if(is.empty(verbose)) stop("Verbose must be boolean, but is empty rigth now!")
 	if(is.empty(debugMode)) stop("DebugMode must be 0,1,2 or 3 but is empty rigth now!")
@@ -9,149 +10,159 @@ check_parameters <- function(functionName, numericParameters, verboseParameter, 
 	if(!pmatch(debugModeParameter,c(0,1,2,3))) stop("debugMode could be either 0,1,2 or 3")
 }
 
-is.empty <- function(elementToBeChecked){
-	if(is.na(elementToBeChecked&&1)||is.na(elementToBeChecked)){
-	return(TRUE)
-	}else{
-	return(FALSE)
-	}
+cEquals <- function(x,splitVal){
+	sum(x==splitVal)
 }
 
-expressionScores_row <- function(expressionMatrix_row, no_expression_value=0, verbose=FALSE, debugMode=0){
-	#Checkpoints
-	check_parameters("expressionScores_row",list(expressionMatrix_row,no_expression_value),verbose,debugMode,verbose,debugMode)
-	#Function itself
-	if(debugMode==1) cat("ExpressionScores_row starting withour errors in checkpoint.\n")
-	if(debugMode==2||debugMode==3) cat("ExpressionScores_row starting withour errors in checkpoint. Paramteres values - expressionMatrix_row:",expressionMatrix_row,"no_expression_value:",no_expression_value,"verbose:",verbose,"debugMode",debugMode,"\n")
-	s <- proc.time()
-	zeroes <- 0
-	above <- 0
-	below <- 0
-	if(debugMode==3) cat("Starting loop.\n")
-	for(i in 1:length(expressionMatrix_row)){
-		sl <- proc.time()
-		if(expressionMatrix_row[i]>no_expression_value){
-			above <- above + 1
-		}else if(expressionMatrix_row[i]<(-no_expression_value)){
-			below <- below + 1
-		}else{
-			zeroes <- zeroes + 1
+cLess <- function(x,splitVal){
+	sum(x>splitVal)
+}
+
+cMore <- function(x,splitVal){
+	sum(x<splitVal)
+}
+
+check <- function(x, overlapInd, margin_range ,splitVal){
+	r <- FALSE
+	if(cEquals(x,splitVal) <= overlapInd){
+		if(abs(cMore(x,splitVal)-cLess(x,splitVal)) < margin_range){
+			r <- TRUE
 		}
-		el<-proc.time()
-		if(debugMode==3) cat("Done element:",i,"this took:",(el-sl)[3],"seconds,","left elements:",length(expressionMatrix_row)-i,"estimated time to finish:",(length(expressionMatrix_row)-i)*(el-s)[3]/i,"seconds.\n")
 	}
-	output <- list(zeroes,above,below)
-	e<-proc.time()
-	if(verbose) cat("Done, expressionScores_row took:",(e-s)[3],"seconds.\n")
-	if(debugMode==3){ 
-		cat("expressionScores_row done, returns output:\n")
-		print(output)
-	}
-	output
+	r
 }
 
-#applies for every row function that counts 0, aboves and belows
-expressionScores <- function(expressionMatrix, no_expression_value=0, verbose=FALSE, debugMode=0){
-	#Checkpoints
-	check_parameters("expressionScores",list(expressionMatrix,no_expression_value),verbose,debugMode,verbose,debugMode)
-	if(no_expression_value<min(expressionMatrix)) stop("no_expression_value too small, lower than minimal value in matrix")
-	#Function itself
-	if(debugMode==1) cat("expressionScores starting withour errors in checkpoint.\n")
-	if(debugMode==2||debugMode==3) cat("expressionScores starting withour errors in checkpoint. Paramteres values- expressionMatrix:",expressionMatrix,"no_expression_value",no_expression_value,"verbose:",verbose,"debugMode",debugMode,"\n")
-	#Function itself
-	s <- proc.time()
-	output <- apply(expressionMatrix,1,expressionScores_row,no_expression_value,verbose,debugMode)
-	e<-proc.time()
-	if(verbose) cat("Done, expressionScores took:",(e-s)[3],"seconds.\n")
-	if(debugMode==3){ 
-		cat("expressionScores done, returns output:\n")
-		print(output)
+switchMatrixValues <- function(Matrix,from1,from2,to1,to2){
+	Matrix[which(Matrix==from1)] <- to1
+	Matrix[which(Matrix==from2)] <- to2
+	Matrix
+}
+
+orderGroup <- function(chromMatrix,verbose=FALSE,debugMode=0){
+	if(verbose)cat("   -> Starting un_order_chromosome_by_seriation\n",file="orderMarkers.log",append=T)
+	cur <- abs(cor(chromMatrix,use="pairwise.complete.obs"))
+	for(i in 1:10){
+		si <- proc.time()
+		if(verbose)cat("      -> Iteration",i,"\n",file="orderMarkers.log",append=T)
+		o <- seriate(t((cur)))
+		cur <- cur[get_order(o),get_order(o)]
+		ei <- proc.time()
+		if(verbose)cat("         -> Iteration",i,"taken:",(ei-si)[3],"seconds. Estimated time left:",(10-i)*(ei-si)[3],"seconds.\n",file="orderMarkers.log",append=T)
 	}
-	output
+	get_order(o)
+}
+
+brassica_genotypes2 <- matrix(as.numeric(switchMatrixValues(brassica_genotypes,"A","B",0,1)),nrow(brassica_genotypes),ncol(brassica_genotypes))
+
+orderMarkers <- function(chrom_matrix,nr_iterations=100,groups=10,verbose=FALSE,debugMode=0,logFile="orderMarkers.log",outputFile="cross2.csv"){
+	#r <- un_best_clustering(chrom_matrix,nr_iterations,groups)
+	cat("",file=logFile)
+	if(verbose){cat("orderMarkers starting.\n\n",file=logFile,append=T)}
+	s <- proc.time()
+	cat("",file=outputFile)
+	cat("phenotype",sep="",file=outputFile,append=T)
+	cat(",,,",sep="",file=outputFile,append=T)
+	cat(paste(runif(48,0,100),collapse=","),sep="",file=outputFile,append=T)
+	cat("\n",sep="",file=outputFile,append=T)
+	chrom_matrix <- matrix(as.numeric(switchMatrixValues(chrom_matrix,"A","B",0,1)),nrow(chrom_matrix),ncol(chrom_matrix))
+	cor_matrix <- cor(t(chrom_matrix), use="pairwise.complete.obs")
+	r <- kmeans(chrom_matrix,groups)
+	sorted <- sort(r[[1]],index.return=T)
+	#res <- NULL
+	start <- 1
+	for(i in 1:groups){
+		sl <- proc.time()
+		if(verbose){cat("   ### Writing chromosome: ",i,"nr of markers:",length(which(r[[1]]==i)),"###\n",file=logFile,append=T)}
+		write.table(cbind( sorted[[1]][which(sorted[[1]]==i)], 1:table(sort(r[[1]]))[i], brassica_genotypes[sorted[[2]][which(sorted[[1]]==i)],]) , file=outputFile , sep="," , quote=F , col.names=F , append=T)
+		el <- proc.time()
+		if(verbose){cat("   ### Writing chromosome:",i,"taken:",(el-sl)[3],"seconds. ###\n\n\n",file=logFile,append=T)}
+	}
+	e <- proc.time()
+	cross <- read.cross("csvr",file=outputFile,genotypes=c("A","B"))
+	class(cross)[1] <- "riself"
+	if(verbose){cat("orderMarkers taken:",(e-s)[3],"seconds.\n",file=logFile,append=T)}
+	if(verbose){cat("Done without errors in:",(e-s)[3],"seconds. Saved log to:",logFile,"\n")}
+	invisible(cross)
+}
+
+
+
+test.appriopriateMarkers <- function(){
+	expressionMatrix <- as.matrix(read.table("Expression_BrassicaRapa_10chr2.txt",sep=""))
+	brassica_genotypes <- appriopriateMarkers(expressionMatrix,margin=2, overlapInd=0, verb=T)
+	dim(brassica_genotypes)
+	cross <- orderMarkers(brassica_genotypes,verbose=T)
+	plot.rf(formLinkageGroups(cross,reorgMarkers=F))
+
 }
 
 #function that choses form the matrix only appropriate markers with specified rules
-appriopriateMarkers <- function(fileName, proportion=50, margin=5, zeros_allowed = 3, no_expression_value=0, genotypeLabel1=0, genotypeLabel2=1, genotypeSplittingValue=0, verbose=FALSE, debugMode=0){
-	#Checkpoints
-	expressionMatrix <- as.matrix(read.table("Expression_BrassicaRapa_10chr2.txt",sep=""))
-	check_parameters("appriopriateMarkers",list(expressionMatrix,no_expression_value,proportion,margin,zeros_allowed),verbose,debugMode,verbose,debugMode)
-	if(proportion < 1 || proportion > 99) stop("Proportion is a percentage (1,99)")
-	if(zeros_allowed < 0 || zeros_allowed > ncol(expressionMatrix)) stop("Zeros_allowed is a number (0,lenght of the row).")
-	if(margin < 0 || margin > proportion) stop("Margin is a percentage (0,proportion)")
-	if(no_expression_value<min(expressionMatrix)) stop("no_expression_value too small, lower than minimal value in matrix")
-	if(no_expression_value>max(expressionMatrix)) stop("no_expression_value too big, higher than maximal value in matrix")
-	if(debugMode==1) cat("appriopriateMarkers starting withour errors in checkpoint.\n")
-	if(debugMode==2||debugMode==3) cat("appriopriateMarkers starting withour errors in checkpoint. Paramteres values- expressionMatrix:",expressionMatrix,"proportion:",proportion,"margin",margin,"zeros_allowed:",zeros_allowed,"no_expression_value:",no_expression_value,"genotypeLabel1:",genotypeLabel1,"genotypeLabel2:",genotypeLabel2,"genotypeSplittingValue:",genotypeSplittingValue,"verbose:",verbose,"debugMode:",debugMode,"\n")
-	#Function itself
+#expressionMatrix -> columns -. indivudlas,rows markers
+#proportion -> Proportion of individuals expected to carrying a certain genotype
+#margin -> Proportion is allowed to varry between this margin (2 sided)
+#splitVal -> Mean expression at which we split the indivuals by
+#overlapInd -> number of individuals that are allowed to overlap (have splitVal)
+#genotypes -> User defined genotypes for the output matrix
+#verbose standrad
+#debugmode standrad
+
+appriopriateMarkers <- function(expressionMatrix, proportion = 50, margin = 5, splitVal = 0, splitValmargin = 0, overlapInd = 0, genotypes = c("A","B"), verbose=FALSE, debugMode=0){
 	s <- proc.time()
-	#needs function providing points
-	points_vector <- expressionScores(expressionMatrix,no_expression_value,verbose,debugMode)
-	output <- NULL
-	for(i in 1:nrow(expressionMatrix)){
-		sl <- proc.time()
-		zero <- points_vector[i][[1]][[1]]
-		non_zero <- dim(expressionMatrix)[2]-zero
-		above_min_below <- abs(points_vector[i][[1]][[2]]-points_vector[i][[1]][[3]])
-		margin_range <- (non_zero*margin)/100
-		if(zero <= zeros_allowed){
-			if(above_min_below < margin_range){
-				output <- c(output,i)
-			}
-		}
-		el <- proc.time()
-		if(verbose) cat("Done with element",i,"took:",(el-sl)[3],", estimated time remining:",(nrow(expressionMatrix)-i) * (el-sl)[3],"\n")
-	}
-	result <- expressionMatrix[output,]
-	geno_matrix <- result
+	if(proportion < 1 || proportion > 99) stop("Proportion is a percentage (1,99)")
+	if(overlapInd < 0 || overlapInd > ncol(expressionMatrix)) stop("overlapInd is a number (0,lenght of the row).")
+	if(margin < 0 || margin > proportion) stop("Margin is a percentage (0,proportion)")
+	if(splitVal<min(expressionMatrix)) stop("splitVal too small, lower than minimal value in matrix")
+	if(splitVal>max(expressionMatrix)) stop("splitVal too big, higher than maximal value in matrix")
+	if(debugMode==1) cat("appriopriateMarkers starting withour errors in checkpoint.\n")
+	if(debugMode==2||debugMode==3) cat("appriopriateMarkers starting withour errors in checkpoint. Paramteres values- expressionMatrix:",expressionMatrix,"proportion:",proportion,"margin",margin,"zeros_allowed:",zeros_allowed,"no_expression_value:",no_expression_value,"genotypeLabel1:",genotypeLabel1,"genotypeLabel2:",genotypeLabel2,"splitVal:",splitVal,"verbose:",verbose,"debugMode:",debugMode,"\n")
+
+	#Selection of the probes matching to the specified parameters
+	margin_range <- (ncol(expressionMatrix)*margin)/100
+	suitedRows <- apply(expressionMatrix,1,check, overlapInd,margin_range,splitVal)#+splitValmargin)
+	expressionMatrix <- expressionMatrix[suitedRows,]
+
 	ep <- proc.time()
 	if(verbose) cat("Selected proper probes, took:",(ep-s)[3],"seconds. Creating genotype matrix.\n")
-	geno_matrix[which(result<genotypeSplittingValue)] <- genotypeLabel1
-	geno_matrix[which(result>genotypeSplittingValue)] <- genotypeLabel2
-	geno_matrix[which(result==genotypeSplittingValue)] <- NA
+	
+	#Transform numeric values to genotypes
+	expressionMatrix[which(expressionMatrix>splitVal)] <- genotypes[1]
+	expressionMatrix[which(expressionMatrix<splitVal)] <- genotypes[2]
+	expressionMatrix[which(expressionMatrix==splitVal)] <- NA
+
 	eg <- proc.time()
 	if(verbose) cat("Created genotype matrix, took:",(eg-ep)[3],"seconds.\n")
-	#we habe now vector which for every row in the matrix says us, how many zeros there are and how many above/below vaules
 	e<-proc.time()
 	if(verbose) cat("Done, appriopiateMarkers took:",(e-s)[3],"seconds.\n")
-	if(is.empty(geno_matrix)) warning("Genotypic matrix for selected conditions is empty.\n")
-	if(debugMode==3){ cat("appriopiateMarkers done, returns geno_matrix:\n")
-	print(geno_matrix)}
-	gc()
-	geno_matrix	
+
+	expressionMatrix
 }
 
-cleanPlus<-function(matrix_to_be_cleaned){
-	for(h in 1:nrow(matrix_to_be_cleaned)){
-		for(w in 1:ncol(matrix_to_be_cleaned)){
-			if(is.na(matrix_to_be_cleaned[h,w])){
-				matrix_to_be_cleaned[h,w] <- as.character("")
-			}else if(matrix_to_be_cleaned[h,w]==0){
-				matrix_to_be_cleaned[h,w] <- as.character("A")
-			}else{
-				matrix_to_be_cleaned[h,w] <- as.character("B")
-			}
-		}
-	}
+#DON"T USE
+cleanPlus<-function(matrix_to_be_cleaned,naValue,genoAValue,genoBValue){
+	matrix_to_be_cleaned[which(is.na(matrix_to_be_cleaned))]<-naValue
+	matrix_to_be_cleaned[which(matrix_to_be_cleaned==0)]<-genoAValue
+	matrix_to_be_cleaned[which(matrix_to_be_cleaned==1)]<-genoBValue
 	matrix_to_be_cleaned
 }
 
-crossParser <- function(genotypicMatrix){
-	genotypicMatrix <- cleanPlus(genotypicMatrix)
-	Tfile <- "output.csv"
-	cat("",file=Tfile)
-	cat("phenotype",sep="",file=Tfile,append=T)
-	cat(",,,",sep="",file=Tfile,append=T)
-	cat(paste(runif(48,0,100),collapse=","),sep="",file=Tfile,append=T)
-	cat("\n",sep="",file=Tfile,append=T)
-	for(i in 1:nrow(genotypicMatrix)){
-		cat(rownames(genotypicMatrix)[i],1,i,sep=",",file = Tfile,append=T)
-		for(w in 1:ncol(genotypicMatrix)-1){
-			cat(genotypicMatrix[i,w],sep="",file = Tfile,append=T)
-			cat(",",sep="",file=Tfile,append=T)
-		}
-		cat(genotypicMatrix[i,ncol(genotypicMatrix)],sep="",file = Tfile,append=T)
-		cat("\n",file = Tfile,append=T)
-	}
+crossParser <- function(genotypicMatrix,outputFile="cross.csv",verbose=FALSE, debugMode=0){
+	genotypicMatrix <- cleanPlus(genotypicMatrix,"-","A","B")
+	if(verbose){cat("crossParser: startin function.\n")}
+	s <- proc.time()
+	cat("",file=outputFile)
+	cat("phenotype",sep="",file=outputFile,append=T)
+	cat(",,,",sep="",file=outputFile,append=T)
+	cat(paste(runif(48,0,100),collapse=","),sep="",file=outputFile,append=T)
+	cat("\n",sep="",file=outputFile,append=T)
+	e1 <- proc.time()
+	if(verbose){cat("crossParser: started file, faked phenotype, strating genotypes. Taken:",(e1-s)[3],"s so far.\n")}
+	if(verbose){cat("crossParser: starting loop, lines to write:",nrow(genotypicMatrix),"\n")}
+	write.table(cbind(1,1:nrow(genotypicMatrix),genotypicMatrix),file=outputFile,sep=",",quote=F,col.names=F,append=T)
+	e <- proc.time()
+	if(verbose){cat("crossParser: finished. Taken:",(e-s)[3],"s. Result written to:",outputFile,"\n")}
+	cross <- read.cross("csvr",file=outputFile,genotypes=c("A","B"))
+	class(cross)[1] <- "riself"
+	invisible(cross)
 }
 
 
@@ -191,10 +202,10 @@ record_format_parser(brassica_genotypes)
 
 setwd("D:/data")
 library(basicQtl)
-brassica_genotypes <- appriopriateMarkers("Expression_BrassicaRapa_10chr2.txt",zeros_allowed=3, no_expression_value=0.05)
+brassica_genotypes <- appriopriateMarkers("Expression_BrassicaRapa_10chr2.txt",zeros_allowed=0, no_expression_value=0.05)
 brassica_genotypes_cor <- cor(t(brassica_genotypes), use="pairwise.complete.obs")
 d <- dist(brassica_genotypes)
 o <- seriate(d)
-brassica_genotypes_cor <- cor((un_ord), use="pairwise.complete.obs")
+brassica_genotypes_cor <- cor(brassicaOrdered, use="pairwise.complete.obs")
 brassica_genotypes_cor <- cor(t(brassica_genotypes)[,get_order(o)], use="pairwise.complete.obs")
 appriopiateMarkers(m,zeros_allowed=0,verbose=TRUE,debugMode=3)
