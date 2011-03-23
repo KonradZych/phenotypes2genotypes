efa3d6d0b705a09bd04e5934f8ebb94627537e5e
KonradZych <dominesum@gmail.com>
2011-03-23 16:58:10 +0100

    change - removed crossMode, doClustering and faking geno/phenotypes, added argument use, for user to choose which geno matrix to use

--- R/genotypesToCross.r
+++ R/genotypesToCross.r
@@ -38,63 +38,43 @@
 # genos - argument passed to read.cross (chars describing genotypes)
 # usage cross <- orderedCross(genotypicMatrix,expressionMatrix)
 
-genotypesToCross <- function(ril, doClustering=FALSE, groups=10, iterations = 100, outputFile="mycross.csv", verbose=FALSE, debugMode=0){
+genotypesToCross <- function(ril, use=c("real","simulated"), doClustering=FALSE, groups=10, iterations = 100, outputFile="mycross.csv", verbose=FALSE, debugMode=0){
 	###CHECKS
 	if(verbose && debugMode==1) cat("genotypesToCross starting.\n")
 	s <- proc.time()
 	
-	#**********CHECKING GENOTYPIC MATRICES*************
-	cat("",file=outputFile)
-	if(!is.null(ril$rils$genotypes$read)){
-		if(!is.null(ril$rils$genotypes$simulated)){
-			#two matrices present
-			crossMode <- 0
-		}
-		#only read from file matrix present
-		crossMode <- 1
-	}else if(!is.null(ril$rils$genotypes$simulated)){
-		#only simulated matrix presen
-		crossMode <- 2
-	}else{
-		cat("genotypesToCross not provided with genotypic matrix, will fake one!\n")
-		fakeGenotypes(outputFile, verbose, debugMode)
-		crossMode <- 3
-	}
 	
 	#**********WRITING PHENOTYPIC DATA TO FILE*************
 	if(!is.null(ril$rils$phenotypes)){
 		#there is phenotypic matrix
+		cat("Writing phenotypic data to cross file\n")
 		writePhenotypes(ril$rils$phenotypes,outputFile, verbose, debugMode)
-	}else if(crossMode!=3){
-		#there is no phenotypic matrix, but there is genotypic matrix
-		fakePhenotypes(outputFile, verbose, debugMode)
 	}else{
 		#there is no phenotypic matrix, neither the genotypic matrix
-		stop("genotypesToCross not provided with genotypic neither phenotypic matrix, stopping\n")
+		stop("genotypesToCross not provided with phenotypic matrix, stopping\n")
 	}
-
+	
 	#**********WRITING GENOTYPIC DATA TO FILE*************
-	if(doClustering){
-		if(crossMode==0){
-			doClustering(ril$rils$genotypes$read,groups,outputFile, verbose, debugMode)
-			doClustering(ril$rils$genotypes$simulated,groups,outputFile, verbose, debugMode)
-		}else if(crossMode==1){
-			doClustering(ril$rils$genotypes$read,groups,outputFile, verbose, debugMode)
-		}else if(crossMode==2){
-			doClustering(ril$rils$genotypes$simulated,groups,outputFile, verbose, debugMode)
+	cat("",file=outputFile)
+	if(use=="real"){
+		if(is.null(ril$rils$genotypes$read)){
+			stop("Use = real chosen, but there is no real genotypic data in ril$rils$genotypes$read\n")
+		}else{
+			cat("Cross object will be written using real genotypic data\n")
+			writeGenotypes(ril$rils$genotypes$read, 1,outputFile, verbose, debugMode)
 		}
-	}else{
-		if(crossMode==0){
-			writeGenotypes(ril$rils$genotypes$read, 1,outputFile, verbose, debugMode)
-			writeGenotypes(ril$rils$genotypes$simulated, 1,outputFile, verbose, debugMode)
-		}else if(crossMode==1){
-			writeGenotypes(ril$rils$genotypes$read, 1,outputFile, verbose, debugMode)
-		}else if(crossMode==2){
+	}
+	else if(use=="simulated"){
+		if(is.null(ril$rils$genotypes$simulated)){
+			stop("Use = simulated chosen, but there is no simulated genotypic data in ril$rils$genotypes$simulated\n")
+		}else{
+			cat("Cross object will be written using simulated genotypic data\n")
 			writeGenotypes(ril$rils$genotypes$simulated, 1,outputFile, verbose, debugMode)
 		}
 	}
 
-	#**********READING CROSS FILE TO R*************
+
+#**********READING CROSS FILE TO R*************
 	cross <- invisible(read.cross("csvr",file=outputFile, genotypes=c(0,1)))
 	class(cross)[1] <- "riself"
 	e <- proc.time()
@@ -111,20 +91,6 @@ writePhenotypes <- function(expressionMatrix, outputFile, verbose=FALSE, debugMo
 	if(verbose && debugMode==2)cat("Writing phenotypes done in:",(el-sl)[3],"seconds.\n")
 }
 
-doClustering <- function(genotypeMatrix, groups, iterations, outputFile, verbose=FALSE, debugMode=0){
-	r <- bestClustering(genotypeMatrix,groups,iterations,verbose=verbose,debugMode=debugMode)
-	r <- kmeans(r,groups)
-	sorted <- sort(r[[1]],index.return=TRUE)
-	for(i in 1:groups){
-		sl <- proc.time()
-		if(verbose && debugMode==1) cat("writeGenotypes starting  for chromome",i,"out of",groups,".\n")
-		writeGenotypes(genotypeMatrix[sorted[[2]][which(sorted[[1]]==i)],], i,outputFile, verbose, debugMode)
-		el <- proc.time()
-	}
-	if(verbose && debugMode==2)cat("doClustering done in:",(el-sl)[3],"seconds.\n")
-}
-
-
 #writeGenotypes - writes to file genotypic data (cross object format)
 #genotypeMatrix - matrix of genotypic data, rows - markers, cols - individuals
 #verbose - standard
@@ -135,16 +101,4 @@ writeGenotypes <- function(genotypeMatrix,chr=1,outputFile,verbose=FALSE,debugMo
 	write.table(cbind(chr,1:nrow(genotypeMatrix),genotypeMatrix),file=outputFile,sep=",",quote=FALSE,col.names=FALSE,append=TRUE)
 	el <- proc.time()
 	if(verbose && debugMode==2) cat("Writing genotypes done in:",(el-sl)[3],"seconds.\n")
-}
-
-fakeGenotypes <- function(len, outputFile, verbose=FALSE, debugMode=0){
-	cat("faked,1,1,",file=outputFile,append=TRUE)
-	cat(paste(sample(c(0,1),100,T),",",sep="",collapse=""),file=outputFile,append=TRUE)
-	cat("\n",file=outputFile,append=TRUE)
-}
-
-fakePhenotypes <- function(len, outputFile, verbose=FALSE, debugMode=0){
-	cat("faked,,,",file=outputFile,append=TRUE)
-	cat(paste(sample(c(0.03,0.04,0.05),100,T),",",sep="",collapse=""),file=outputFile,append=TRUE)
-	cat("\n",file=outputFile,append=TRUE)
 }
