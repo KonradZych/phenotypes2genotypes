771742336c0ba4a8cddc5c72e7be1d69a385edcb
KonradZych <dominesum@gmail.com>
2011-03-29 12:02:02 +0200

    documentation - more clear inside code, removed few minor mistakes from help file

--- R/toGenotypes.r
+++ R/toGenotypes.r
@@ -1,4 +1,4 @@
-#################################################################################
+############################################################################################################
 #
 # toGenotypes.R
 #
@@ -22,13 +22,23 @@
 #     at http://www.r-project.org/Licenses/GPL-3
 #
 # Contains: toGenotypes
-#           transformIndividual, zero, cEquals, cMore, cLess, checkExpression
+#           convertToGenotypes.internal, splitRow.internal, filterGenotypes.internal, filterRow.internal 
 #
-#################################################################################
+############################################################################################################
 
+############################################################################################################
 #toGenotypes: Function that chooses from the matrix only appropriate markers with specified rules
-
-toGenotypes <- function(ril, use=c("real","simulated"), treshold=0.01, overlapInd = 0, proportion = 50, margin = 5, verbose=FALSE, debugMode=0){
+# 
+# ril - Ril type object, must contain parental phenotypic data.
+# use - Which genotypic matrix should be saved to file, real - supported by user and read from file, simulated - made by toGenotypes.s
+# treshold - If Rank Product pval for gene is lower that this value, we assume it is being diff. expressed.
+# overlapInd - Number of individuals that are allowed in the overlap
+# proportion - Proportion of individuals expected to carrying a certain genotype 
+# margin - Proportion is allowed to varry between this margin (2 sided)
+# verbose - Be verbose
+# debugMode - 1: Print our checks, 2: print additional time information
+############################################################################################################
+toGenotypes <- function(ril, use=c("real","simulated"), treshold=0.01, overlapInd = 0, proportion = 50, margin = 15, verbose=FALSE, debugMode=0){
 	#*******CHECKS*******
 	if(proportion < 1 || proportion > 99) stop("Proportion is a percentage (1,99)")
 	#if(overlapInd < 0 || overlapInd > ncol(expressionMatrix)) stop("overlapInd is a number (0,lenght of the row).")
@@ -37,7 +47,7 @@ toGenotypes <- function(ril, use=c("real","simulated"), treshold=0.01, overlapIn
 	
 	#*******CONVERTING CHILDREN PHENOTYPIC DATA TO GENOTYPES*******
 	s1 <- proc.time()
-	ril <- convertToGenotypes.internal(ril, treshold,verbose, debugMode)
+	ril <- convertToGenotypes.internal(ril, treshold, verbose, debugMode)
 	e1 <- proc.time()
 	if(verbose && debugMode==2)cat("Converting phenotypes to genotypes in:",(e1-s1)[3],"seconds.\n")
 	
@@ -59,6 +69,14 @@ toGenotypes <- function(ril, use=c("real","simulated"), treshold=0.01, overlapIn
 	#Order markers
 }
 
+############################################################################################################
+#convertToGenotypes.internal: function splitting differentially expressed markers into two genotypes
+# 
+# ril - Ril type object, must contain parental phenotypic data.
+# treshold - If Rank Product pval for gene is lower that this value, we assume it is being diff. expressed.
+# verbose - Be verbose
+# debugMode - 1: Print our checks, 2: print additional time information 
+############################################################################################################
 convertToGenotypes.internal <- function(ril,treshold,verbose=FALSE,debugMode=0){
 	if(verbose && debugMode==1) cat("convertToGenotypes starting.\n")
 	m <- NULL
@@ -79,6 +97,14 @@ convertToGenotypes.internal <- function(ril,treshold,verbose=FALSE,debugMode=0){
 	invisible(ril)
 }
 
+############################################################################################################
+#splitRow.internal: subfunction of convertToGenotypes.internal, splitting one row
+# 
+# x - name of currently processed row
+# rils - matrix of up/down regulated genes in rils
+# parental - matrix of up/down regulated genes in parents
+# genotypes - values genotypic matrix will be filled with
+############################################################################################################
 splitRow.internal <- function(x,rils,parental,genotypes){
 	result <- rep(0,length(rils[x,]))
 	splitVal <- mean(parental[which(rownames(parental) == x),])
@@ -88,6 +114,16 @@ splitRow.internal <- function(x,rils,parental,genotypes){
 	invisible(result)
 }
 
+############################################################################################################
+#filterGenotypes.internal: removing from genotypic matrix genes that are no passing specified requirments
+# 
+# ril - Ril type object, must contain parental phenotypic data.
+# overlapInd - Number of individuals that are allowed in the overlap
+# proportion - Proportion of individuals expected to carrying a certain genotype 
+# margin - Proportion is allowed to varry between this margin (2 sided)
+# verbose - Be verbose
+# debugMode - 1: Print our checks, 2: print additional time information
+############################################################################################################
 filterGenotypes.internal <- function(ril, overlapInd=0, proportion=50, margin=5, verbose=FALSE,debugMode=0){
 	if(verbose && debugMode==1) cat("filterGenotypes starting.\n")
 	result <- apply(ril$rils$genotypes$simulated,1,filterRow.internal,overlapInd=overlapInd,proportion=proportion, margin=margin)
@@ -95,6 +131,14 @@ filterGenotypes.internal <- function(ril, overlapInd=0, proportion=50, margin=5,
 	invisible(ril)
 }
 
+############################################################################################################
+#filterRow.internal: subfunction of filterGenotypes.internal, filtering one row
+# 
+# genotypeRow - currently processed row
+# overlapInd - Number of individuals that are allowed in the overlap
+# proportion - Proportion of individuals expected to carrying a certain genotype 
+# margin - Proportion is allowed to varry between this margin (2 sided)
+############################################################################################################
 filterRow.internal <- function(genotypeRow, overlapInd, proportion, margin){
 	if(sum(is.na(genotypeRow))>overlapInd) return(0)
 	above <- sum(genotypeRow==1)/length(genotypeRow) * 100
