847de24f2353dd6b575aef00dca6668498cca650
KonradZych <dominesum@gmail.com>
2011-03-29 15:32:16 +0200

    add - plotting routine fotr children, parental not working properly because of stupid R bug

--- R/plotingRoutines.r
+++ R/plotingRoutines.r
@@ -27,14 +27,38 @@
 ##################################################################################################
 
 
-plotParental <- function(ril,plotMode=c("rank","compare"),treshold=0.05,groupLabels=c(0,0,1,1),verbose=FALSE,debugMode=0){
-	if(plotMode=="rank"){
-		colPal <- matrix("gray",nrow(ril$parental$phenotypes),ncol(ril$parental$phenotypes))
-		colPal[which(ril$parental$pval[1]<treshold),] <- "red"
-		colPal[which(ril$parental$pval[2]<treshold),] <- "black"
-	}else if(plotMode=="compare"){
-		colPal <- matrix("black",nrow(ril$parental$phenotypes),ncol(ril$parental$phenotypes))
-		colPal[,which(groupLabels==1)] <- "red"
+############################################################################################################
+#plotParental: not that useful, but stays for now
+# 
+############################################################################################################
+plotParental <- function(ril, markers=1:100, groupLabels=c(0,0,1,1), verbose=FALSE, debugMode=0){
+	stop("due to stupid R bug, no you shouldn't use it\n")
+	parental <- ril$parental$phenotypes[markers,]
+	# R BUG - really stupid, xlim should be (x1,x2), but if I put it this way I get error that there is "," that
+	# is not allowed...
+	#plot(x=markers[1], y=parental[which(groupLabels==0)[1]], xlim=(1,length(markers)), col="red" )
+	for(i in 1:length(markers)){
+		for(j in 1:ncol(parental[,which(groupLabels==0)])){
+			points(x=i,y=parental[j],col="red")
+		}
+		for(j in 1:ncol(parental[,which(groupLabels==1)])){
+			points(x=i,y=parental[j],col="blue")
+		}
 	}
-	plot(ril$parental$phenotypes[1,],col=colPal[1,])
+	points(apply(parental,1,mean),col="green", pch=95, cex=3)
+}
+
+############################################################################################################
+#plotChildrenExpression: boxplot of data for selected markers + points of parental mean for each marker
+# 
+# ril - Ril type object, must contain parental phenotypic data.
+# markers - markers to be printed numbers or names 
+# ... - passed to boxplot
+############################################################################################################
+plotChildrenExpression <- function(ril, markers=1:100, ...){
+	boxplot(t(ril$rils$phenotypes[markers,]))
+	parental <- ril$parental$phenotypes[which(rownames(ril$parental$phenotypes) %in% rownames(ril$rils$phenotypes[markers,])),]
+	points(apply(parental,1,mean),col="green", pch=95, cex=3)
+	points(apply(parental,1,max),col="blue", pch=24, cex=1)
+	points(apply(parental,1,min),col="red", pch=25, cex=1)
 }
