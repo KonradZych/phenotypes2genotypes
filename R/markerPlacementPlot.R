886f4fb7f2701eaf669d9877f51c8ebe8c216e85
KonradZych <dominesum@gmail.com>
2011-12-15 13:27:07 +0100

    markerPlacementPlot - now showing qtl/corr

--- R/markerPlacementPlot.R
+++ R/markerPlacementPlot.R
@@ -26,19 +26,51 @@
 #
 ############################################################################################################
 
-markerPlacementPlot <- function(population, cross){
+markerPlacementPlot <- function(population, placeUsing=c("qtl","correlation"), cross){
   if(missing(cross)){
     cross <- genotypesToCross.internal(population, "simulated")
   }
-  cormatrix <- map2mapCorrelationMatrix(cross,population)
-  s <- NULL
-  p <- seq(1,5,0.1)
-  for(x in p){
-    maximums <- apply(abs(cormatrix),2,max)
-    means <- apply(abs(cormatrix),2,mean)
-    sds <- apply(abs(cormatrix),2,sd)
-    selected <- which(maximums > (means+x*sds))
-    s <- c(s,length(selected))
-  }
+  if(placeUsing=="correlation"){
+    cormatrix <- map2mapCorrelationMatrix(cross,population)
+    s <- NULL
+    p <- seq(1,5,0.1)
+    for(x in p){
+      maximums <- apply(abs(cormatrix),2,max)
+      means <- apply(abs(cormatrix),2,mean)
+      sds <- apply(abs(cormatrix),2,sd)
+      selected <- which(maximums > (means+x*sds))
+      s <- c(s,length(selected))
+    }
   plot(p,s,type='o',main="Number of markers placed",xlab="corThreshold",ylab="# of markers")
+  }else{
+    genotypes <- population$offspring$genotypes$real
+    markers <- markernames(cross)
+    phenotypes <- pull.geno(cross)[,markers]
+    results <- NULL
+    rnames <- NULL
+    cnt <- 1
+    for(x in 1:ncol(phenotypes)){
+      results <- rbind(results,apply(genotypes,1, 
+        function(geno){
+          linmod <- lm(phenotypes[,x] ~ geno)
+          -log10(anova(linmod)[[5]][1])
+        }
+      ))
+      rnames <- c(rnames,colnames(phenotypes)[x])
+      cnt <- cnt +1
+    }
+  rownames(results) <- rnames
+  singleqtl <- NULL
+  noqtl <- NULL
+  multipleqtl <- NULL
+  for(tr in seq(0,20,0.5)){
+    nqtls <- apply(results,1,function(x){sum(x>tr)})
+    singleqtl <- c(singleqtl,sum(nqtls==1))
+    noqtl <- c(noqtl,sum(nqtls==0))
+    multipleqtl <- c(multipleqtl,sum(nqtls>1))
+  }
+  plot(noqtl,type='l',col="red",main="Number of markers placed",xlab="threshold",ylab="# of markers")
+  points(singleqtl,type='l',col="green")
+  points(multipleqtl,type='l',col="blue")
+  }
 }
