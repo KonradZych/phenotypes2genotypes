e5b8d3f89dc02458d17c36f56d045ab82e4f4c8f
DannyArends <Danny.Arends@gmail.com>
2012-05-26 13:01:55 +0200

    [R] empty file for multimap

---
+++ R/plot.multimap.r
@@ -0,0 +1,17 @@
+#
+# plot.multimap.R
+#
+# copyright (c) 2010 Danny Arends, konrad Zych and Ritsert C. Jansen
+# last modified May, 2012
+# first written May, 2012
+# 
+# R functions to plot multiple maps in comparison
+#
+
+plot.multimap <- function(maps){
+
+}
+
+test.plot.multimap <- function(){
+
+}
