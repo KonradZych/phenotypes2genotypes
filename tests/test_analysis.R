5fa34192d154c2273760104f45ac02a9b9869689
KonradZych <dominesum@gmail.com>
2012-11-06 14:42:42 +0100

    removing trash from test file

--- tests/test_analysis.R
+++ tests/test_analysis.R
@@ -10,34 +10,13 @@ population <- find.diff.expressed(population)
 population <- generate.biomarkers(population, threshold=0.1, verbose=T, debug=2)
 population <- scan.qtls(population,map="physical",verbose=T,step=2)
 
-#with parental data
-#populationNoParental <- create.population(children,offspring_genotypes=genotypes,maps_physical=map,verbose=TRUE)
-#populationNoParental <- find.diff.expressed(populationNoParental)
-#populationNoParental <- generate.biomarkers(populationNoParental, threshold=0.1, verbose=T, debug=2)
-#populationNoParental <- scan.qtls(populationNoParental,map="physical",verbose=T,step=2)
-
 ####THREE WAYS TO ASSIGN CHROMOSOMES
 set.seed(101010)
 cross_newmap <- cross.denovo(population,n.chr=16,map="physical",comparisonMethod=sumMajorityCorrelation,reOrder=TRUE,use.orderMarkers=FALSE,verbose=TRUE,debugMode=2)
-#set.seed(101010)
-#cross_newmap <- cross.denovo(population, n.chr=16,map="physical",comparisonMethod=majorityCorrelation,reOrder=TRUE,verbose=TRUE,debugMode=2)
-#set.seed(101010)
-#cross_newmap <- cross.denovo(population, n.chr=16,map="physical",comparisonMethod=meanCorrelation,reOrder=TRUE,verbose=TRUE,debugMode=2)
-cross_newmap <- smooth.geno(cross_newmap,3)
-
-set.seed(101010)
-cross_newmap <- cross.denovo(populationNoParental,n.chr=16,map="physical",comparisonMethod=sumMajorityCorrelation,reOrder=TRUE,use.orderMarkers=FALSE,verbose=TRUE,debugMode=2)
-#set.seed(101010)
-#cross_newmap <- cross.denovo(population, n.chr=16,map="physical",comparisonMethod=majorityCorrelation,reOrder=TRUE,verbose=TRUE,debugMode=2)
-#set.seed(101010)
-#cross_newmap <- cross.denovo(population, n.chr=16,map="physical",comparisonMethod=meanCorrelation,reOrder=TRUE,verbose=TRUE,debugMode=2)
 cross_newmap <- smooth.geno(cross_newmap,3)
 
 cross_saturated <- cross.saturate(population,map="physical",verbose=TRUE,debugMode=2)
 cross_saturated <- smooth.geno(cross_saturated,3)
-cross_saturated <- cross.saturate(populationNoParental,map="physical",verbose=TRUE,debugMode=2)
-cross_saturated <- smooth.geno(cross_saturated,3)
-
 
 #To output folder
 write.cross(cross_newmap,file="yeast_cross_newmap.csv",format="csv")
