9f4d14048ab135c188a6d9f55de7612ea1706493
KonradZych <dominesum@gmail.com>
2013-01-10 16:45:32 +0100

    new plotting function
    some updates

--- R/power.plot.r
+++ R/power.plot.r
@@ -14,7 +14,7 @@
 # OUTPUT:
 #  An object of class cross
 #
-power.plot <- function(cross1,cross2,qtl.thr=5,n.pheno=500,...){
+power.plot <- function(cross1,cross2,qtl.thr=5,n.pheno=500,verbose=FALSE,...){
   if(missing(cross1)){
     stop("No object of class cross.")
   }
@@ -33,19 +33,24 @@ power.plot <- function(cross1,cross2,qtl.thr=5,n.pheno=500,...){
   if(nphe(cross1)!=nphe(cross2)){
      stop("Difference in phenotype number between crosses.")
   }
-  if(!is.numeric(n.pheno) || n.pheno<0 || n.pheno>nphe(cross1)){
-    stop("qtl.thr should be a numeric value between 0 and nphe(cross1).")
+  if(!is.numeric(n.pheno) || n.pheno<2 || n.pheno>nphe(cross1)){
+    stop("n.pheno should be a numeric value between 2 and nphe(cross1).")
   }
   markers <- sample(1:nphe(cross1),n.pheno)
   res1 <- scanone(cross1,pheno.col=markers,...)
   res2 <- scanone(cross2,pheno.col=markers,...)
-  maxes1 <- apply(res1[,3:ncol(resO)],2,max)
-  maxes2 <- apply(res2[,3:ncol(resS)],2,max)
+  maxes1 <- apply(res1[,3:ncol(res1)],2,max)
+  maxes2 <- apply(res2[,3:ncol(res2)],2,max)
+  if(verbose){
+    vals <- cbind(markers,maxes1,maxes2)
+    colnames(vals) <- c("#phenotype","max LOD in cross1","max LOD in cross2")
+    print(vals)
+  }
   plot(maxes1,maxes2,pch=20,xlab="LOD scores in the original cross",ylab="LOD scores in the saturated cross")
   abline(0,1,col="red")
   abline(v=qtl.thr,lty=2,col="grey")
   abline(h=qtl.thr,lty=2,col="grey")
   significant<-which(maxes1>qtl.thr)
   cat(sum(maxes1[significant]<maxes2[significant]),"which is:",sum(maxes1[significant]<maxes2[significant])/length(significant),"% of significant qtls show increase in power\n")
-  invisible(sum(maxes1[significant]<maxes2[significant])/length(significant))
+  invisible(vals)
 }
