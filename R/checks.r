2f256d342499a413493b60e101a3cafaf0675e5e
DannyArends <Danny.Arends@gmail.com>
2013-04-02 13:22:21 +0200

    Again, minor updates, and spacing

--- R/checks.r
+++ R/checks.r
@@ -18,9 +18,9 @@
 #
 numericCheck.internal <- function(objectToBeChecked, allow.na=FALSE){
   converted <- as.numeric(as.matrix(objectToBeChecked))
-  if(any(is.na(converted)){
+  if(any(is.na(converted))){
     if(!(allow.na)) return(FALSE)
-    if(sum(is.na(converted == sum(is.na((objectToBeChecked)))) return(TRUE)
+    if(sum(is.na(converted)) == sum(is.na((objectToBeChecked)))) return(TRUE)
     return(FALSE)
   }
   return(TRUE)
