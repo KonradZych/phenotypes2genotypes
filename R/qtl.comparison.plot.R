d106dbcc8d60276b919b456da4130303d5d1ca74
Konrad Zych <dominesum@gmail.com>
2013-05-11 20:01:18 +0200

    formatting

--- R/qtl.comparison.plot.R
+++ R/qtl.comparison.plot.R
@@ -1,15 +1,13 @@
 #
-# markersCorPlot.R
+# qtl.comparison.plot.R
 #
-# Copyright (c) 2010-2012 GBIC: Danny Arends, Konrad Zych and Ritsert C. Jansen
-# last modified May, 2012
-# first written Mar, 2011
-# Contains: markersCorPlot, ascendingMaptoJigSawMap, getChrOffsets.internal
-#           getMarkerOffsets, getMarkerOffsetsFromMap, chromCorMatrix
-#           getPopulationOffsets.internal 
+# Copyright (c) 2010-2013 GBIC: Danny Arends, Konrad Zych and Ritsert C. Jansen
+# last modified May, 2013
+# first written Nov, 2012
+# Contains: qtl.comparison.plot
 #
 
-# markersCorPlot
+# qtl.comparison.plot
 #
 # DESCRIPTION:
 #  function to create new map and save it in cross object
@@ -21,12 +19,12 @@
 # OUTPUT:
 #  An object of class cross
 #
-qtl.comparison.plot <- function(cross, cross2, chr, ...){
-  if(missing(cross))   stop("Provide two objects of class cross!\n")
+qtl.comparison.plot <- function(cross1, cross2, chr, ...){
+  if(missing(cross1))   stop("Provide two objects of class cross!\n")
   if(missing(cross2))  stop("Provide two objects of class cross!\n")
-  if(!(any(class(cross)=="cross")))   stop("Provide two objects of class cross!\n")
+  if(!(any(class(cross1)=="cross")))   stop("Provide two objects of class cross!\n")
   if(!(any(class(cross2)=="cross")))  stop("Provide two objects of class cross!\n")
-  res1 <- scanone(cross,...)
+  res1 <- scanone(cross1,...)
   res2 <- scanone(cross2,...)
   if((missing(chr))){
     chr <- 1
@@ -35,10 +33,8 @@ qtl.comparison.plot <- function(cross, cross2, chr, ...){
     chr <- chr[1]
     warning("Function can plot only one chromosome at the time, plotting first of selected chromosomes!")
   }
-  if(!(chr%in%1:min(nchr(cross),nchr(cross2)))){
+  if(!(chr%in%1:min(nchr(cross1),nchr(cross2)))){
     stop("Chromosome asked is not present in the cross object\n")
   }
-  plot(res1,res2,col=c("orange","black"),chr=n,main="Figure 2 - QTL profiles comparison.")
-  axis(4,chrPoints[1:(length(chrPoints)-1)]/scaler,paste("Chr",1:(min(nchr(cross),nchr(cross2))),sep=" "))
-
+  plot(res1,res2,col=c("orange","black"),chr=chr,main="Figure 2 - QTL profiles comparison.")
 }
