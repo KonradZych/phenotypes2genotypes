25353b4140c842a8ba7e104182afda858381b1ca
Konrad Zych <dominesum@gmail.com>
2013-05-12 17:43:52 +0200

    bugfix

--- R/add.to.population.R
+++ R/add.to.population.R
@@ -133,8 +133,8 @@ add.to.populationSubPheno.internal <- function(population, dataObject, dataType=
   if(!(is.null(dataObject))&&!is.null(dim(dataObject))){ #Check whether rows are numeric/convertable to numeric
     rows <- unlist(lapply(c(1:nrow(dataObject)), function(curRow,dataObject,verbose){
       if(verbose&&curRow%%1000==0) cat("Processing row:",curRow,"\n")
-      if(numericCheck.internal(dataObject[curRow,],allow.na=TRUE)) return(curRow)
-      return(NULL)
+      if(numericCheck.internal(dataObject[curRow,],allow.na=TRUE)) return(NULL)
+      return(curRow)
     },dataObject,verbose))    
     
     if(!(is.null(rows))){ #Removing faulty rows
