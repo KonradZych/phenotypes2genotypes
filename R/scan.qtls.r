c65681e65b18275cefb131373291f45f447ef07e
Konrad Zych <dominesum@gmail.com>
2013-05-12 17:22:31 +0200

    bugfix

--- R/scan.qtls.r
+++ R/scan.qtls.r
@@ -141,6 +141,7 @@ twoGenosModel <- function(genoRow, markerRow, maxGeno, env, useEnv){
   curRes <- -log10(anova(lm(markerRow~env+maxGeno+genoRow))[[5]])
   output <- curRes[2]
   if(useEnv) output <- curRes[3]
+  print(output)
   invisible(useEnv)
 }
 
