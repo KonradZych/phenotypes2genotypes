2d94cfffc472f26081941c48352db3abb473c384
Konrad Zych <dominesum@gmail.com>
2013-05-12 18:18:42 +0200

    updating test

--- tests/test_analysis.R
+++ tests/test_analysis.R
@@ -5,10 +5,10 @@ genotypes <- read.csv(file="genotypes.csv",header=TRUE,row.names=1)
 map <- read.csv(file="map.csv",header=TRUE,row.names=1)
 
 #with parental data
-population <- create.population(children,parents,c(0,0,0,0,0,0,1,1,1,1,1,1),genotypes,maps_physical=map,verbose=TRUE)
+population <- create.population(children,parents,c(0,0,0,0,0,0,1,1,1,1,1,1),genotypes,verbose=TRUE)
 population <- find.diff.expressed(population)
 population <- generate.biomarkers(population, threshold=0.1, verbose=T, debug=2)
-population <- scan.qtls(population,map="physical",verbose=T,step=2)
+population <- scan.qtls(population,verbose=T,step=0)
 
 ####THREE WAYS TO ASSIGN CHROMOSOMES
 set.seed(101010)
