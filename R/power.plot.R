348569838fdef8f7a1a666ec6b9321f8afce8be8
KonradZych <dominesum@gmail.com>
2013-05-22 15:34:37 +0200

    small improvement
    not  we can plot the scores without recalculating them

--- R/power.plot.R
+++ R/power.plot.R
@@ -14,38 +14,44 @@
 # OUTPUT:
 #  An object of class cross
 #
-power.plot <- function(cross1,cross2,qtlThr=5,nPheno=500,verbose=FALSE,...){
-  if(missing(cross1)){
-    stop("No object of class cross.")
+power.plot <- function(cross1,cross2,scores,qtlThr=5,nPheno=500,verbose=FALSE,...){
+  if(missing(scores)){
+    if(missing(cross1)){
+      stop("No object of class cross.")
+    }
+    if(missing(cross2)){
+      stop("No object of class cross.")
+    }
+    if (!any(class(cross1) == "cross")){
+      stop("Input should have class \"cross\".")
+    }
+    if (!any(class(cross2) == "cross")){
+      stop("Input should have class \"cross\".")
+    }
+    if(!is.numeric(qtlThr) || qtlThr<0){
+      stop("qtlThr should be a numeric value bigger than 0.")
+    }
+    if(nphe(cross1)!=nphe(cross2)){
+       stop("Difference in phenotype number between crosses.")
+    }
+    if(!is.numeric(nPheno) || nPheno<2 || nPheno>nphe(cross1)){
+      stop("nPheno should be a numeric value between 2 and nphe(cross1).")
+    }
+    if(is.null(cross1$geno[[1]]$prob)){
+      cross1 <- calc.genoprob(cross1)
+    }
+    if(is.null(cross2$geno[[1]]$prob)){
+      cross2 <- calc.genoprob(cross2)
+    }
+    markers  <- sample(1:nphe(cross1),nPheno)
+    res1     <- scanone(cross1,pheno.col=markers,...)
+    res2     <- scanone(cross2,pheno.col=markers,...)
+  }else if(class(scores)=="scores"){
+    res1     <- scores[[1]]
+    res2     <- scores[[2]]
+  }else{
+    stop("This is not a valuid scores object!")
   }
-  if(missing(cross2)){
-    stop("No object of class cross.")
-  }
-  if (!any(class(cross1) == "cross")){
-    stop("Input should have class \"cross\".")
-  }
-  if (!any(class(cross2) == "cross")){
-    stop("Input should have class \"cross\".")
-  }
-  if(!is.numeric(qtlThr) || qtlThr<0){
-    stop("qtlThr should be a numeric value bigger than 0.")
-  }
-  if(nphe(cross1)!=nphe(cross2)){
-     stop("Difference in phenotype number between crosses.")
-  }
-  if(!is.numeric(nPheno) || nPheno<2 || nPheno>nphe(cross1)){
-    stop("nPheno should be a numeric value between 2 and nphe(cross1).")
-  }
-  if(is.null(cross1$geno[[1]]$prob)){
-    cross1 <- calc.genoprob(cross1)
-  }
-  if(is.null(cross2$geno[[1]]$prob)){
-    cross2 <- calc.genoprob(cross2)
-  }
-  markers  <- sample(1:nphe(cross1),nPheno)
-  qtlThr2 <- qtlThr
-  res1     <- scanone(cross1,pheno.col=markers,...)
-  res2     <- scanone(cross2,pheno.col=markers,...)
   maxes1   <- apply(res1[,3:ncol(res1)],2,max)
   maxes2   <- apply(res2[,3:ncol(res2)],2,max)
   if(verbose){
@@ -56,15 +62,17 @@ power.plot <- function(cross1,cross2,qtlThr=5,nPheno=500,verbose=FALSE,...){
   colorNr   <- as.numeric(maxes1>maxes2)+1
   colorCols <- c("green","red")
   plot(maxes1,maxes2,pch=20,xlab="LOD scores in the original cross",ylab="LOD scores in the saturated cross",main="Figure 3 - QTL detection power.",col=colorCols[colorNr])
-  abline(qtlThr2-qtlThr,1,col="red") #diagonal line
+  abline(qtlThr-qtlThr,1,col="red") #diagonal line
   abline(v=qtlThr,lty=2,col="grey")   #dotted line showing threshold used for x axis
-  abline(h=qtlThr2,lty=2,col="grey")  #dotted line showing threshold used for y axis
+  abline(h=qtlThr,lty=2,col="grey")  #dotted line showing threshold used for y axis
   significant  <- which(maxes1>qtlThr)  #nr of significant QTLs in cross 1
-  significant2 <- which(maxes2>qtlThr2) #nr of significant QTLs in cross 2
+  significant2 <- which(maxes2>qtlThr) #nr of significant QTLs in cross 2
   stronger     <- sum(maxes1[significant]<(maxes2[significant])) #nr of QTLs showubg increase in power
   cat(length(significant),"significant qtls\n")
   cat(stronger,"which is:",stronger/length(significant),"% of significant qtls show increase in power\n")
   cat(length(significant2)-length(significant),"new QTLs found\n") # nr of gained QTLs
-  invisible(list(res1,res2))
+  scores <- list(res1,res2)
+  class(scores) <- "scores"
+  invisible(scores)
 }
 
